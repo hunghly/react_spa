796cb15d56e14b005b9be9e4c85d313021890da0
hunghly <hunghly@gmail.com>
2020-09-08 12:39:40 -0500

    added a inc/dec button to test out reducer

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, {useReducer} from "react";
 
 const Home = () => {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div>
       <h2>HELLO</h2>
@@ -12,8 +15,24 @@ const Home = () => {
         nascetur ridiculus mus.
       </p>
       <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+      Count: {state.count}
+      <button onClick={() => dispatch({type: 'decrement'})}>-</button>
+      <button onClick={() => dispatch({type: 'increment'})}>+</button>
     </div>
   );
 };
+
+const initialState = {count: 0};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return {count: state.count + 1};
+    case 'decrement':
+      return {count: state.count - 1};
+    default:
+      throw new Error();
+  }
+}
 
 export default Home;
